87a5c59b114b62c5849014b324eadfed8419945c
nandobfer <nandobfer@yahoo.com.br>
2023-12-07 14:18:01 -0300

    update: loading

--- src/pages/Verification.tsx
+++ src/pages/Verification.tsx
@@ -2,7 +2,7 @@ import React, { ChangeEvent, useEffect, useRef, useState } from "react"
 import BallOne from "../assets/Peça 1 - Bolinha.webp"
 import BallThree from "../assets/Peça 3 - Bolinha.webp"
 import Selo from "../assets/Peça 1.1 - Selo Natal.png"
-import { Box } from "@mui/material"
+import { Box, LinearProgress } from "@mui/material"
 import { colors } from "../styles/colors"
 import { PaperBall } from "../components/PaperBall"
 import { ButtonBongrano } from "../components/ButtonBongrano"
@@ -25,10 +25,11 @@ export const Verification: React.FC<VerificationProps> = ({}) => {
     const validCode = "ABCDE" // Exemplo de código válido
     const inputRefs = useRef<HTMLInputElement[]>([])
     const [code, setCode] = useState<string[]>(Array(5).fill(""))
+    const [loading, setLoading] = useState(false)
 
     const initialValues: VerifyForm = {
         id: user?.id || 0,
-        code: "",
+        code: ""
     }
     const handleKeyDown = (index: number) => (event: React.KeyboardEvent<HTMLInputElement>) => {
         if (event.key === "Backspace" && code[index] === "" && index > 0) {
@@ -55,8 +56,10 @@ export const Verification: React.FC<VerificationProps> = ({}) => {
     }
 
     const validateCode = (inputCode: string) => {
+        setLoading(true)
         io.emit("user:verify", { id: user?.id, code: inputCode })
     }
+
     useEffect(() => {
         io.on("application:status:approved", () => {
             if (user) {
@@ -64,10 +67,12 @@ export const Verification: React.FC<VerificationProps> = ({}) => {
                 snackbar({ severity: "success", text: "Indique seus amigos" })
                 navigate("/indicate")
             }
+            setLoading(false)
         })
         io.on("application:aproval:error", (data) => {
             const errorMessage = data.error ? data.error : "Falha no cadastro!"
             snackbar({ severity: "error", text: errorMessage[0].toUpperCase() + errorMessage.slice(1) })
+            setLoading(false)
         })
 
         return () => {
@@ -75,6 +80,7 @@ export const Verification: React.FC<VerificationProps> = ({}) => {
             io.off("application:aproval:error")
         }
     }, [])
+
     useEffect(() => {
         inputRefs.current[0].focus()
     }, [])
@@ -82,23 +88,20 @@ export const Verification: React.FC<VerificationProps> = ({}) => {
     useEffect(() => {
         window.scroll(0, 0)
     }, [])
-    return (
+
+    return user ? (
         <Box sx={{ width: "100%", height: "100%", overflowY: "auto", gap: "4vw", flexDirection: "column" }}>
             <PaperBall>
                 <img src={BallOne} alt="" style={{ width: "45vw" }} />
-                <p style={{ textAlign: "center", fontWeight: "600", fontSize: "3.8vw" }}>
-                    Atualize Seus Dados e Fique por Dentro!
-                </p>
+                <p style={{ textAlign: "center", fontWeight: "600", fontSize: "3.8vw" }}>Atualize Seus Dados e Fique por Dentro!</p>
                 <p style={{ textAlign: "center", color: colors.terciary, fontSize: "2.8vw" }}>
-                    Queremos garantir que você não perca nenhuma novidade! Por favor, atualize seus dados para continuar
-                    recebendo informações exclusivas e ofertas especiais da Bongrano. É rápido e fácil - apenas confirme seu
-                    nome completo, endereço, e-mail e WhatsApp. Lembre-se, suas informações estão seguras conosco!
+                    Queremos garantir que você não perca nenhuma novidade! Por favor, atualize seus dados para continuar recebendo informações
+                    exclusivas e ofertas especiais da Bongrano. É rápido e fácil - apenas confirme seu nome completo, endereço, e-mail e WhatsApp.
+                    Lembre-se, suas informações estão seguras conosco!
                 </p>
                 <p style={{ textAlign: "center", color: colors.terciary, fontSize: "2.8vw", padding: "2vw" }}>
-                    Enviamos um código de verificação para o telefone{" "}
-                    <span style={{ fontWeight: "bold" }}>(41) 99999-9999</span>. Por favor insira-o abaixo. Caso não tenha
-                    recebido, também enviamos o código para o email:{" "}
-                    <span style={{ fontWeight: "bold" }}>email@dominio.com.br</span>
+                    Enviamos um código de verificação para o telefone <span style={{ fontWeight: "bold" }}>{user.whatsapp}</span>. Por favor insira-o
+                    abaixo. Caso não tenha recebido, também enviamos o código para o email: <span style={{ fontWeight: "bold" }}>{user.email}</span>
                 </p>
                 <Box sx={{ p: "4vw", gap: "2.5vw" }}>
                     {code.map((_, index) => (
@@ -114,13 +117,12 @@ export const Verification: React.FC<VerificationProps> = ({}) => {
                         />
                     ))}
                 </Box>
+                {loading && <LinearProgress color="primary" sx={{ width: "90%", borderRadius: "5vw" }} />}
             </PaperBall>
 
             <img src={Selo} alt="" />
             <PaperBall>
-                <p style={{ width: "100%", fontWeight: "600", textAlign: "left", fontSize: "3.8vw" }}>
-                    Regras de participação
-                </p>
+                <p style={{ width: "100%", fontWeight: "600", textAlign: "left", fontSize: "3.8vw" }}>Regras de participação</p>
                 <Rules />
             </PaperBall>
             <Box sx={{ justifyContent: "space-between" }}>
@@ -129,5 +131,5 @@ export const Verification: React.FC<VerificationProps> = ({}) => {
                 </ButtonBongrano>
             </Box>
         </Box>
-    )
+    ) : null
 }
